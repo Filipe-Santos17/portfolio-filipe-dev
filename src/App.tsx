6f5500faff08f0a075e35847f4f48c430c63bccd
Filipe <filipemard@gmail.com>
2024-01-10 16:57:13 -0300

    test i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useState } from "react"
+import { useTranslation } from "react-i18next"
+
 export default function App() {
+  const { t, i18n } = useTranslation()
+
+  const [lang, setLang] = useState(i18n.language)
+
+  function setLanguage() {
+    const language = lang === 'en' ? 'pt' : 'en'
+    i18n.changeLanguage(language)
+    setLang(language)
+  }
+
   return (
-    <div className="text-3xl font-bold underline">App</div>
+    <>
+      <div className="text-3xl font-bold">{t('text')}</div>
+      <button onClick={setLanguage}>change</button>
+    </>
   )
 }
--- src/libs/i18next.ts
+++ src/libs/i18next.ts
@@ -1,10 +1,13 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import en from "../locale/en.json"
+import pt from "../locale/pt.json"
+
 i18next.use(initReactI18next).init({
   resources:{
-    pt:{},
-    en:{},
+    pt,
+    en,
   },
-  lng: 'pt',
+  lng: 'en',
 })
