fdb47dfacab4b0a7865611dd6e563ad21db37e72
Filipe <filipemard@gmail.com>
2024-01-11 14:44:44 -0300

    fix a dumb mistake

--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -1,4 +1,4 @@
-import { TypeChangeModalState } from "@/types";
+import { TypeChangeModalState } from "../types";
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 
