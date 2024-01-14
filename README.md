acd6465c4b5488fb64e6b949b625c08ec7616d80
Filipe <filipemard@gmail.com>
2024-01-14 20:13:19 -0300

    add ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Portfolio Dev
+# My Portfolio Dev
+
+[Click here to See](https://portfolio-filipe-dev.vercel.app/)
+
+## 📱 Image:
+
+![AppPrint](/printHome.png "App print screen")
+
+## :four_leaf_clover: Technologies used
+
+This project is being developed with the following technologies.
+
+-   [TypeScript](https://www.typescriptlang.org/docs/)
+-   [React](https://react.dev/)
+-   [Vite](https://vitejs.dev/)
+-   [TailwindCSS](https://tailwindcss.com/)
+-   [React - i18N](https://react.i18next.com/)
+
+[Filipe Santos on Linkedin](https://www.linkedin.com/in/filipemarquesdeveloper/)
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,6 @@ export default function Header({ changeModal }: TypeChangeModalState) {
     <header className="w-full flex items-center mb-12 justify-between px-4 py-6">
       <div>
         <h5 className="dark:text-white">Filipe Dev</h5>
-        {/*hover que aparece os icones com links das minhas redes sociais e estilizar esse texto */}
       </div>
       <ul className="hidden items-center justify-between gap-8 md:flex">
         <LinkTopicList text={t('header.home')} linkPath="/" />
@@ -24,8 +23,8 @@ export default function Header({ changeModal }: TypeChangeModalState) {
       <div className="hidden items-center gap-4 md:flex">
         <BtnActions />
       </div>
-      <div>
-        <button className="flex flex-col md:hidden gap-1 border-none outline-none" onClick={openModal}>
+      <div className="block md:hidden">
+        <button className="flex flex-col gap-1 border-none outline-none" onClick={openModal}>
           <li className="w-6 h-[.12rem] outline-none bg-black dark:bg-white border-none rounded-4xl block"></li>
           <li className="w-6 h-[.12rem] outline-none bg-black dark:bg-white border-none rounded-4xl block"></li>
           <li className="w-6 h-[.12rem] outline-none bg-black dark:bg-white border-none rounded-4xl block"></li>
